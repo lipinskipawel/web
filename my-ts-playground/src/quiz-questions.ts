7f8be1e5d055c599a171969edfbb61f19a7defb9
Pawel Lipinski <lipinskijob@gmail.com>
2026-07-14 08:57:26 +0200

    Randomize questions

--- my-ts-playground/src/quiz-questions.ts
+++ my-ts-playground/src/quiz-questions.ts
@@ -5,17 +5,36 @@ export interface Question {
 	correct: string
 }
 
+const shuffle = (array: Question[]) => {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+};
+
 export function getQuestions(): Array<Question> {
-	return [
+	return shuffle([
 		{
 			"question": "some thing?",
 			"answers": ["A", "B", "C"],
 			"correct": "B"
 		},
 		{
+			"question": "some more questions?",
+			"answers": ["house", "car", "bike"],
+			"correct": "car"
+		},
+		{
+			"question": "what colour is sky?",
+			"answers": ["blue", "red", "dark"],
+			"correct": "blue"
+		},
+		{
 			"question": "some another thing?",
 			"answers": ["AA", "BB", "CC"],
 			"correct": "CC"
 		}
-	]
+	]);
 }
